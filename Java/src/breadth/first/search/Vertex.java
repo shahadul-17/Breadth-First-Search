bcc388007b44f3346e58feb1d2b5c2091d3c71b1
Md. Shahadul Alam Patwary <shahadul_alam_17@outlook.com>
2016-12-21 04:37:37 +0600

    Update Vertex.java

--- Java/src/breadth/first/search/Vertex.java
+++ Java/src/breadth/first/search/Vertex.java
@@ -1,22 +1,12 @@
 package breadth.first.search;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 public class Vertex {
 	
 	public int index, distance = Integer.MAX_VALUE;
 	public boolean visited = false;
 	
 	public Vertex parent;
-	
-	public static PriorityQueue<Vertex> priorityQueue = new PriorityQueue<Vertex>(new Comparator<Vertex>() {
-
-		@Override
-		public int compare(Vertex vertex_1, Vertex vertex_2) {
-			return vertex_1.index - vertex_2.index;
-		}
-	});
+	public static Queue queue;
 	
 	@Override
 	public String toString() {
@@ -28,7 +18,7 @@ public class Vertex {
 			element = element.parent;
 		}
 		
-		return (char) (element.index + 97) + text;
+		return Main.toCharacter(element.index) + text;
 	}
 	
 }
